d97654601289b131bcd99b60fc5dac19370ca579
Leo <workspace2707@yandex.ru>
2024-01-18 22:18:11 +0300

    created tabs interactive and footer and swiper

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,12 @@ const selectChoices = document.querySelectorAll('.header__select');
 // show/hide choice
 
 for (let infoBtn of infoBtns) {
-    infoBtn.addEventListener('click', select)
+  infoBtn.addEventListener('click', select)
 }
 
 function select(event) {
-    event.stopPropagation()
-    this.parentNode.querySelector('.header__select').classList.toggle('none');
+  event.stopPropagation()
+  this.parentNode.querySelector('.header__select').classList.toggle('none');
 }
 
 // hide all chocies
@@ -18,13 +18,67 @@ function select(event) {
 document.addEventListener('click', hideSelect)
 
 function hideSelect() {
-    for (let selectChoice of selectChoices) {
-        selectChoice.classList.add('none')
-    }
+  for (let selectChoice of selectChoices) {
+    selectChoice.classList.add('none')
+  }
 }
 
 // hide choices doesn't work for choices
 
 for (let selectChoice of selectChoices) {
-    selectChoice.addEventListener('click', (e) => {e.stopPropagation()})
+  selectChoice.addEventListener('click', (e) => { e.stopPropagation() })
 }
+
+//swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  slidesPerView: 4,
+  spaceBetween: 42,
+  // loop: true,
+  // freeMode: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#nextButton',
+    prevEl: '#prevButton',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// tabs
+
+const tabsBtns = document.querySelectorAll('[data-tab]');
+const tabsProducts = document.querySelectorAll('[data-tab-value]');
+
+for (let tabsBtn of tabsBtns) {
+
+  tabsBtn.addEventListener('click', function() {
+    tabsBtns.forEach((elem) => elem.classList.remove('tab__controls__button--active'));
+    this.classList.add('tab__controls__button--active');
+
+    for (let tabsProduct of tabsProducts) {
+      if (this.dataset.tab == 'all') {
+        tabsProduct.classList.remove('none');
+      } else {
+        if (tabsProduct.dataset.tabValue === this.dataset.tab) {
+          tabsProduct.classList.remove('none')
+        } else {
+          tabsProduct.classList.add('none');
+        }
+      }
+    }
+    swiper.update();
+  })
+}
+
